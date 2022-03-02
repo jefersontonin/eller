9a7c0e10fbff32f07ee8e624297f0c457c10fcac
Jeferson Tonin <jefersontonin77@gmail.com>
2022-03-02 18:37:27 -0300

    little adjustments on vertical spacing

--- src/components/HomeButton.vue
+++ src/components/HomeButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button class="text-white w-full h-12 border-4 border-white rounded-full uppercase text-sm font-bold">
+    <button class="text-white w-full h-12 border-2 border-white rounded-md uppercase text-sm font-bold">
       <a
         href="http://www.instagram.com/eller.semijoias"
         >
