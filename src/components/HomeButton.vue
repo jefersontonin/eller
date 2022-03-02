4cf0ced8cea3ded737f292b222b36965c63dfca9
Jeferson Tonin <jefersontonin77@gmail.com>
2022-03-02 18:21:04 -0300

    Update HomeButton.vue

--- src/components/HomeButton.vue
+++ src/components/HomeButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col mb-12"></div>
+  <div class=""></div>
   <button class="text-white w-full h-12 border-4 border-white rounded-full uppercase text-sm font-bold">
     <a
       href="http://www.instagram.com/eller.semijoias"
