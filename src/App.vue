aec46b8ec3000aab33f4557731b1fd21cb257a5c
Jeferson Tonin <jefersontonin77@gmail.com>
2022-03-02 18:05:34 -0300

    added home button and promo components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import HomeBackground from './components/HomeBackground.vue';
 import HomeLogo from './components/HomeLogo.vue';
+import HomePromo from './components/HomePromo.vue';
+import HomeButton from './components/HomeButton.vue';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 </script>
@@ -8,6 +10,8 @@ import HomeLogo from './components/HomeLogo.vue';
 <template>
   <home-background>
     <home-logo></home-logo>
+    <home-promo></home-promo>
+    <home-button></home-button>
   </home-background>
 </template>
 
---
+++ src/components/HomeButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col mb-12"></div>
+  <button class="text-white w-full h-12 border-4 border-white rounded-full uppercase text-sm font-bold">
+    <a
+      href="http://www.instagram.com/eller.semijoias"
+      >
+      Clique e conheça nossos produtos
+    </a>
+  </button>
+</template>
